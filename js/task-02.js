a4718aa693ffacfcfbe80f12ac67ceb87ae32abf
AntoninaRadova <110526254+AntoninaRadova@users.noreply.github.com>
2023-02-20 10:06:47 +0200

    Update task-02.js

--- js/task-02.js
+++ js/task-02.js
@@ -7,11 +7,27 @@ const ingredients = [
   "Condiments",
 ];
 
+// Пошук списку
 const list = document.querySelector("#ingredients");
+
+// Пустий масив
 const listItemsArray = [];
+
+// Перебір кожного елементу через forEach
 ingredients.forEach((ingredient) => {
+  
+  // Створення елементу
   const listItem = document.createElement("li");
+
+  // Додавання текстового контенту до кожного елементу
   listItem.textContent = ingredient;
+
+  // Додавання класу
   listItem.classList.add("item");
+
+  // Додавання елемента в масив
   listItemsArray.push(listItem);
 });
+
+// Масив всередині списка
+list.append(...listItemsArray);
