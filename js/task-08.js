930aed7f55668bf24569461307913c4e75962630
AntoninaRadova <110526254+AntoninaRadova@users.noreply.github.com>
2023-02-21 13:31:07 +0200

    Update task-08.js

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,29 @@
+// Пошук формми
+const form = document.querySelector(".login-form");
 
+// Додавання слухача події
+form.addEventListener("submit", handleSubmit);
+
+// Відправка форми
+function handleSubmit(event) {
+  event.preventDefault();
+
+  const {
+    elements: { email, password },
+  } = event.currentTarget;
+
+  // Всі поля заповнені?
+  if (email.value === "" || password.value === "") {
+    return alert("Заповни всі поля!");
+  }
+
+  // Про користувача
+  const userInfo = {
+    email: email.value,
+    password: password.value,
+  };
+
+  console.log(userInfo);
+
+  event.currentTarget.reset();
+}
