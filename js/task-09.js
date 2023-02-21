327034121e5704ae35118c0f17c93ab410e947a1
AntoninaRadova <110526254+AntoninaRadova@users.noreply.github.com>
2023-02-21 13:32:28 +0200

    Update task-09.js

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,16 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+// Пошук елементів
+const body = document.querySelector("body");
+const changeColorBtn = document.querySelector("button.change-color");
+const spanColor = document.querySelector("span.color");
+
+// Додаємо слухача подій
+changeColorBtn.addEventListener("click", (element) => {
+  
+  let color = getRandomHexColor();
+  body.style.backgroundColor = color;
+  spanColor.textContent = color;
+});
